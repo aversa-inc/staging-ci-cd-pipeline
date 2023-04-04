d103343628626770c60c7a1e3b639691bd89b2b8
GhostIA <sarahf5676@gmail.com>
2023-04-04 12:19:26 -0700

    initial commit

---
+++ cdk-v2/lib/ecs_cdk-stack.ts
@@ -0,0 +1,247 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+
+import { Construct } from 'constructs';
+
+export class EcsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const githubUserName = new cdk.CfnParameter(this, "githubUserName", {
+        type: "String",
+        description: "Github username for source code repository"
+    })
+
+    const githubRepository = new cdk.CfnParameter(this, "githubRespository", {
+        type: "String",
+        description: "Github source code repository",
+        default: "amazon-ecs-fargate-cdk-v2-cicd" 
+    })
+
+    const githubPersonalTokenSecretName = new cdk.CfnParameter(this, "githubPersonalTokenSecretName", {
+        type: "String",
+        description: "The name of the AWS Secrets Manager Secret which holds the GitHub Personal Access Token for this project.",
+        default: "/aws-samples/amazon-ecs-fargate-cdk-v2-cicd/github/personal_access_token" 
+    })
+    //default: `${this.stackName}`
+    const vpc = new ec2.Vpc(this, "FargateInSessionService", {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "ingress",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "application",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        },
+      ],
+    });
+
+    const loadbalancer = new ApplicationLoadBalancer(this, "lb", {
+      vpc,
+      internetFacing: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+    });
+
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc,
+      clusterName: "fargate-node-cluster",
+    });
+    const ecrRepo = new ecr.Repository(this, 'ecrRepo', {
+    	repositoryName: "fargate-insession-service",
+    });
+    const executionRole = new iam.Role(this, "ExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonECSTaskExecutionRolePolicy"
+        ),
+      ],
+    });
+
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "FargateNodeService",
+      {
+        cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+          containerName: "insession-service",
+          family: "fargate-node-task-defn",
+          containerPort: 8080,
+          executionRole,
+        },
+        cpu: 256,
+        memoryLimitMiB: 512,
+        desiredCount: 2,
+        serviceName: "fargate-insession-service",
+        taskSubnets: vpc.selectSubnets({
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        }),
+        loadBalancer: loadbalancer,
+      }
+    );
+    
+
+    /**
+     * create a new vpc with single nat gateway
+     */
+    /* where do these constants come from? 6, 10, 60? */
+
+
+
+    const gitHubSource = codebuild.Source.gitHub({
+      owner: githubUserName.valueAsString,
+      repo: githubRepository.valueAsString,
+      webhook: true, // optional, default: true if `webhookfilteres` were provided, false otherwise
+      webhookFilters: [
+        codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH).andBranchIs('main'),
+      ], // optional, by default all pushes and pull requests will trigger a build
+    });
+
+    // codebuild - project
+    const project = new codebuild.Project(this, 'myProject', {
+      projectName: `${this.stackName}`,
+      source: gitHubSource,
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_2,
+        privileged: true
+      },
+      environmentVariables: {
+        'cluster_name': {
+          value: `${cluster.clusterName}`
+        },
+        'ecr_repo_uri': {
+          value: `${ecrRepo.repositoryUri}`
+        }
+      },
+      badge: true,
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+	    pre_build: {
+            	commands: [
+                  './gradlew clean && ./gradlew build',
+              	  'env',
+              	  'export tag=latest'
+            ]
+          },
+          build: {
+            commands: [
+              `docker build -t $ecr_repo_uri:$tag .`,
+              '$(aws ecr get-login --no-include-email)',
+              'docker push $ecr_repo_uri:$tag'
+            ]
+          },
+          post_build: {
+            commands: [
+              'echo "in post-build stage"',
+              'cd ..',
+              "printf '[{\"name\":\"insession-service\",\"imageUri\":\"%s\"}]' $ecr_repo_uri:$tag > imagedefinitions.json",
+              "pwd; ls -al; cat imagedefinitions.json"
+            ]
+          }
+        },
+        artifacts: {
+          files: [
+            'imagedefinitions.json'
+          ]
+        }
+      })
+    });
+
+
+
+    // ***pipeline actions***
+
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+    const nameOfGithubPersonTokenParameterAsString = githubPersonalTokenSecretName.valueAsString
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'github_source',
+      owner: githubUserName.valueAsString,
+      repo: githubRepository.valueAsString,
+      branch: 'main',
+      oauthToken: cdk.SecretValue.secretsManager(nameOfGithubPersonTokenParameterAsString),
+      output: sourceOutput
+    });
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'codebuild',
+      project: project,
+      input: sourceOutput,
+      outputs: [buildOutput], // optional
+    });
+
+    const manualApprovalAction = new codepipeline_actions.ManualApprovalAction({
+      actionName: 'approve',
+    });
+
+    const deployAction = new codepipeline_actions.EcsDeployAction({
+      actionName: 'deployAction',
+      service: fargateService.service,
+      imageFile: new codepipeline.ArtifactPath(buildOutput, `imagedefinitions.json`)
+    });
+
+
+
+    // pipeline stages
+
+    new codepipeline.Pipeline(this, 'myecspipeline', {
+      stages: [
+        {
+          stageName: 'source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'build',
+          actions: [buildAction],
+        },
+        {
+          stageName: 'approve',
+          actions: [manualApprovalAction],
+        },
+        {
+          stageName: 'deploy-to-ecs',
+          actions: [deployAction],
+        }
+      ]
+    });
+
+
+    ecrRepo.grantPullPush(project.role!)
+    project.addToRolePolicy(new iam.PolicyStatement({
+      actions: [
+        "ecs:describecluster",
+        "ecr:getauthorizationtoken",
+        "ecr:batchchecklayeravailability",
+        "ecr:batchgetimage",
+        "ecr:getdownloadurlforlayer"
+        ],
+      resources: [`${cluster.clusterArn}`],
+    }));
+
+
+    new cdk.CfnOutput(this, "image", { value: ecrRepo.repositoryUri+":latest"} )
+    new cdk.CfnOutput(this, 'loadbalancerdns', { value: fargateService.loadBalancer.loadBalancerDnsName });
+
+  }
+
+
+
+}
